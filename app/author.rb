127b7b0cfe17ec488bbfe96fda9f443f80d3da72
lesayobruce <lesayobruce419@gmail.com>
2023-02-19 21:07:00 +0300

    completed compiling codes for author file

--- app/author.rb
+++ app/author.rb
@@ -1,3 +1,23 @@
 class author
     attr_accessor :name
+    def initialize(name)
+        @name = name
+    end
+
+    def articles
+        Article.all.filter{|article| article.author == @name}
+    end
+
+    def magazine
+    article.map{|article| article.magazine}.uniq
+    end
+
+    def add_article(magazine, title)
+        Article,new(self, magazine, title)
+    end
+    def topic_areas
+        magazine.map{|magazine| magazine.category}.uniq
+    end
+
+    
 end
