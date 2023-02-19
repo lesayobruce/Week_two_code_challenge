83f8065998f5bf855dca34db9450f51be3e0330c
lesayobruce <lesayobruce419@gmail.com>
2023-02-19 20:53:34 +0300

    compiled codes for article file

---
+++ app/article.rb
@@ -0,0 +1,20 @@
+class Article
+
+    attr_accessor :magazine, :title
+    @@all = []
+
+    def initialize(author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author
+        @author.name
+    end
+end
