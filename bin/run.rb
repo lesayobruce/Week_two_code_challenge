5a7c1c36f9b8c7ca1c226586950f1170a3c2ea06
lesayobruce <lesayobruce419@gmail.com>
2023-02-21 12:23:37 +0300

    completed the code challenge

--- bin/run.rb
+++ bin/run.rb
@@ -1,16 +1,17 @@
 require_relative '../config/environment'
+
 # Create some authors
-author1 = Author.new("Jane Smith")
-author2 = Author.new("John Doe")
+author1 = Author.new("Ladysha")
+author2 = Author.new("John Ronald")
 # Create some magazines
-magazine1 = Magazine.new("Vogue", "Fashion")
-magazine2 = Magazine.new("National Geographic", "Science")
+magazine1 = Magazine.new("Truth meter", "Deception")
+magazine2 = Magazine.new("Study the stars", "Universe affairs")
 # Add articles for the authors
-author1.add_article(magazine1, "The Latest Fashion Trends")
+author1.add_article(magazine1, "The week's untamed story")
 author1.add_article(magazine2, "New Discoveries in Science")
-author2.add_article(magazine2, "The Future of Technology")
+author2.add_article(magazine2, "The unveiled astronomy")
 # Find a magazine by name
-magazine = Magazine.find_by_name("National Geographic")
+magazine = Magazine.find_by_name("Truth Meter")
 # Print some information
 puts "Authors:"
 Author.all.each do |author|
@@ -20,7 +21,8 @@ puts "\nMagazines:"
 Magazine.all.each do |magazine|
   puts "#{magazine.name} has #{magazine.contributors.count} contributors and article titles are: #{magazine.article_titles.join(", ")}"
 end
-puts "\nContributing authors for #{magazine.name}:"
-magazine.contributing_authors.each do |author|
-  puts "- #{author.name}"
-end
+# puts "\nContributing authors for #{magazine.name}:"
+# magazine.contributing_authors.each do |author|
+#   puts "- #{author.name}"
+# end
+# binding.pry
